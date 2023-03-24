10698df0a891755d76b19cd44204d17f84cc90e0
davidmd98 <davidmd98@gmail.com>
2023-03-24 14:56:59 +0100

    Video list update

--- src/main/java/isdcm/restapp/daos/VideoDao.java
+++ src/main/java/isdcm/restapp/daos/VideoDao.java
@@ -33,84 +33,53 @@ public class VideoDao {
     public List<Video> getVideosByAuthor(String author) throws SQLException{
         String query = "SELECT * FROM VIDEOS WHERE author LIKE ?";
         try(PreparedStatement preparedStatement = connection.prepareStatement(query);){
-            List<Video> videos = new ArrayList<>();
             preparedStatement.setString(1, String.format("%%%s%%",author));
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String title = resultSet.getString("title");
-                Date creationDate = resultSet.getDate("creation_date");
-                Time duration = resultSet.getTime("duration");
-                int reproductions = resultSet.getInt("reproductions");
-                String description = resultSet.getString("description");
-                String url = resultSet.getString("url");
-                Video video = new Video(title, author, creationDate, duration, reproductions, description, url, false);
-                videos.add(video);
-            }
-            return videos;
+            return getVideoList(resultSet);
         } 
     }
     
     public List<Video> getVideos() throws SQLException{
         String query = "SELECT * FROM VIDEOS";
         try(PreparedStatement preparedStatement = connection.prepareStatement(query);){
-            List<Video> videos = new ArrayList<>();
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String title = resultSet.getString("title");
-                String author = resultSet.getString("author");
-                Date creationDate = resultSet.getDate("creation_date");
-                Time duration = resultSet.getTime("duration");
-                int reproductions = resultSet.getInt("reproductions");
-                String description = resultSet.getString("description");
-                String url = resultSet.getString("url");
-                Video video = new Video(title, author, creationDate, duration, reproductions, description, url, false);
-                videos.add(video);
-            }
-            return videos;
+            return getVideoList(resultSet);
         } 
     }
     
     public List<Video> getVideosByTitle(String titleInput)throws SQLException{
         String query = "SELECT * FROM VIDEOS WHERE title LIKE ?";
         try(PreparedStatement preparedStatement = connection.prepareStatement(query);){
-            List<Video> videos = new ArrayList<>();
             preparedStatement.setString(1, String.format("%%%s%%", titleInput));
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String title = resultSet.getString("title");
-                String author = resultSet.getString("author");
-                Date creationDate = resultSet.getDate("creation_date");
-                Time duration = resultSet.getTime("duration");
-                int reproductions = resultSet.getInt("reproductions");
-                String description = resultSet.getString("description");
-                String url = resultSet.getString("url");
-                Video video = new Video(title, author, creationDate, duration, reproductions, description, url, false);
-                videos.add(video);
-            }
-            return videos;
+            return getVideoList(resultSet);
         } 
     }
     
     public List<Video> getVideosByCreationDate(Date startDate, Date endDate)throws SQLException{
         String query = "SELECT * FROM VIDEOS WHERE creation_date BETWEEN ? AND ?";
         try(PreparedStatement preparedStatement = connection.prepareStatement(query);){
-            List<Video> videos = new ArrayList<>();
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             preparedStatement.setString(1, formatter.format(startDate));
             preparedStatement.setString(1, formatter.format(endDate));
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String title = resultSet.getString("title");
-                String author = resultSet.getString("author");
-                Date creationDate = resultSet.getDate("creation_date");
-                Time duration = resultSet.getTime("duration");
-                int reproductions = resultSet.getInt("reproductions");
-                String description = resultSet.getString("description");
-                String url = resultSet.getString("url");
-                Video video = new Video(title, author, creationDate, duration, reproductions, description, url, false);
-                videos.add(video);
-            }
-            return videos;
+            return getVideoList(resultSet);
         } 
+    }
+    private List<Video> getVideoList(ResultSet resultSet)throws SQLException{
+        List<Video> videos = new ArrayList<>();
+        while (resultSet.next()) {
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            Date creationDate = resultSet.getDate("creation_date");
+            Time duration = resultSet.getTime("duration");
+            int reproductions = resultSet.getInt("reproductions");
+            String description = resultSet.getString("description");
+            String url = resultSet.getString("url");
+            boolean isLocal = resultSet.getBoolean("isLocal");
+            Video video = new Video(title, author, creationDate, duration, reproductions, description, url, isLocal);
+            videos.add(video);
+        }
+        return videos;
     }
 }
